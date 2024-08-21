a607a95a2dd06f51dc912a005434d503ae5ce7d4
Mohd Saqlain <mohdsaqlain598@gmail.com>
2024-08-21 15:29:03 +0530

    modified Dashboard

--- my-app/src/Pages/Overview.js
+++ my-app/src/Pages/Overview.js
@@ -86,8 +86,8 @@ const Overview = () => {
           </Button>
         </div>
         <div className="mb-3 d-flex justify-content-start  mb-5">
-          <Button variant="link" style={{ fontSize: '0.85rem', color: 'gray', marginBottom:'50px' }}>Monthly</Button>
-          <Button variant="link" style={{ fontSize: '0.85rem', color: 'black',marginBottom:'50px', backgroundColor: '#f1ebf9' }}>Yearly</Button>
+          <Button variant="link" style={{ fontSize: '0.85rem', color: 'gray', marginBottom:'70px' }}>Monthly</Button>
+          <Button variant="link" style={{ fontSize: '0.85rem', color: 'black',marginBottom:'70px', backgroundColor: '#f1ebf9' }}>Yearly</Button>
         </div>
         <Bar data={data} options={options} />
       </Card.Body>
